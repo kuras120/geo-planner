bf49ac4800f6a11617f3c68589888c6e3b89ffdb
Wojciech Kur <wojciech.kur96@gmail.com>
2026-08-09 22:42:15 +0200

    chore: Centralize Gradle plugins in version catalog (#5)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,18 @@
+plugins {
+    // this is necessary to avoid the plugins to be loaded multiple times
+    // in each subproject's classloader
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.spring) apply false
+    alias(libs.plugins.kotlin.jpa) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.spring.boot) apply false
+    alias(libs.plugins.spring.dependency.management) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
 allprojects {
     group = "com.simplydevit"
     version = "0.0.1-SNAPSHOT"
-
-    repositories {
-        mavenCentral()
-    }
 }
 
 tasks.register("buildAll") {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,15 @@
 pluginManagement {
-    plugins {
-        kotlin("jvm") version "2.3.21"
-        kotlin("plugin.spring") version "2.3.21"
-        kotlin("plugin.jpa") version "2.3.21"
-        kotlin("kapt") version "2.3.21"
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
-        id("org.springframework.boot") version "4.1.0"
-        id("io.spring.dependency-management") version "1.1.7"
-        id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        mavenCentral()
     }
 }
 
